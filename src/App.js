cba08367520c7ec489ece6852e2af509c1e36a96
Sappy2001 <saptangsumodak@gmail.com>
2023-07-11 18:00:42 +0530

    Adding browser router and making chages to deploy with github pages

--- src/App.js
+++ src/App.js
@@ -1,45 +1,98 @@
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
+import {BrowserRouter} from "react-router-dom";
 
-import './App.css';
-import React, { Component } from 'react'
-import Navbar from './components/Navbar';
-import News from './components/News' ;
-import LoadingBar from 'react-top-loading-bar'
-import {
-  BrowserRouter as Router,Route, Switch} from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 export default class App extends Component {
-  c="john";
-  state={
-    progress:0
-  }
-  setProgress=(progress)=>{
-    this.setState({progress:progress})
-  }
-  apiKey=process.env.REACT_APP_NEWS_API;
-  render() {
-   
 
-    return (
-      <>
-      <Router>
-        <Navbar/>
-        <LoadingBar
-        height={3}
-        color='#f11946'
-        progress={this.state.progress}      
-      />
-        <Switch>
-          <Route exact path="/"><News setProgress={this.setProgress} key="general" country="in" apiKey={this.apiKey} category="general" />
-          </Route>
-          <Route exact path="/business"><News setProgress={this.setProgress} key="business" country="in" apiKey={this.apiKey} category="business" /></Route>
- <Route exact path="/entertainment"><News setProgress={this.setProgress} key="entertainment" country="in" apiKey={this.apiKey} category="entertainment" /></Route>
- 
- <Route exact path="/health"><News setProgress={this.setProgress} key="health" country="in"apiKey={this.apiKey} category="health" /></Route>
- <Route exact path="/science"><News setProgress={this.setProgress} key="science" country="in"apiKey={this.apiKey} category="science" /></Route>
- <Route exact path="/sports"><News setProgress={this.setProgress} key="sports" country="in"apiKey={this.apiKey} category="sports" /></Route>
- <Route exact path="/technology"><News setProgress={this.setProgress} key="technology" country="in"apiKey={this.apiKey} category="technology" /></Route>
-        </Switch>
-      </Router>
-      </>
-    )
-  }
+	state = {
+		progress: 0,
+	};
+	setProgress = (progress) => {
+		this.setState({ progress: progress });
+	};
+	apiKey = process.env.REACT_APP_NEWS_API;
+	render() {
+		return (
+			<>
+				<BrowserRouter basename="/newsers">
+					<Navbar />
+					<LoadingBar
+						height={3}
+						color="#f11946"
+						progress={this.state.progress}
+					/>
+					<Switch>
+						<Route exact path="/">
+							<News
+								setProgress={this.setProgress}
+								key="general"
+								country="in"
+								apiKey={this.apiKey}
+								category="general"
+							/>
+						</Route>
+						<Route exact path="/business">
+							<News
+								setProgress={this.setProgress}
+								key="business"
+								country="in"
+								apiKey={this.apiKey}
+								category="business"
+							/>
+						</Route>
+						<Route exact path="/entertainment">
+							<News
+								setProgress={this.setProgress}
+								key="entertainment"
+								country="in"
+								apiKey={this.apiKey}
+								category="entertainment"
+							/>
+						</Route>
+
+						<Route exact path="/health">
+							<News
+								setProgress={this.setProgress}
+								key="health"
+								country="in"
+								apiKey={this.apiKey}
+								category="health"
+							/>
+						</Route>
+						<Route exact path="/science">
+							<News
+								setProgress={this.setProgress}
+								key="science"
+								country="in"
+								apiKey={this.apiKey}
+								category="science"
+							/>
+						</Route>
+						<Route exact path="/sports">
+							<News
+								setProgress={this.setProgress}
+								key="sports"
+								country="in"
+								apiKey={this.apiKey}
+								category="sports"
+							/>
+						</Route>
+						<Route exact path="/technology">
+							<News
+								setProgress={this.setProgress}
+								key="technology"
+								country="in"
+								apiKey={this.apiKey}
+								category="technology"
+							/>
+						</Route>
+					</Switch>
+				</BrowserRouter>
+			</>
+		);
+	}
 }
